10f23a484a1328782af9568d12370eb00b21759f
Sderengo1219 <S.Derengowski@gmail.com>
2026-07-01 13:19:48 -0400

    added the ability to run python files

---
+++ functions/run_python_file.py
@@ -0,0 +1,44 @@
+import os
+import subprocess
+
+def run_python_file(
+    working_directory: str, file_path: str, args: list[str] | None = None
+) -> str:
+
+    try:    
+        working_dir_abs = os.path.abspath(working_directory)
+
+        full_path = os.path.join(working_dir_abs, file_path)
+        full_path = os.path.normpath(full_path)
+
+        if os.path.commonpath([working_dir_abs, full_path]) != working_dir_abs:
+            return f'Error: Cannot execute "{file_path}" as it is outside the permitted working directory'
+        elif not os.path.isfile(full_path):
+            return f'Error: "{file_path}" does not exist or is not a regular file'
+        elif not full_path.endswith(".py"):
+            return f'Error: "{file_path}" is not a Python file'
+
+        command = ["python", full_path]
+
+        if args:
+            command.extend(args)
+
+        process_complete = subprocess.run(command, cwd=working_dir_abs, capture_output=True, text=True, timeout=30)
+
+        output_parts = []
+
+        if process_complete.returncode != 0:
+            output_parts.append(f"Process exited with code {process_complete.returncode}")
+        if not process_complete.stdout and not process_complete.stderr:
+            output_parts.append("No output produced")
+        if process_complete.stdout:
+            output_parts.append(f"STDOUT: {process_complete.stdout}")
+        if process_complete.stderr:
+            output_parts.append(f"STDERR: {process_complete.stderr}")
+
+        output_string = "\n".join(output_parts)
+
+        return output_string
+
+    except Exception as e:
+        return f"Error: executing Python file: {e}"
